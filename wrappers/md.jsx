91d0e5291b46d88502213917135660b1d731c79f
Ville Miekk-oja <Ville.Miekkoja@gmail.com>
2016-01-14 14:42:51 +0200

    Change page title to Digitransit

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -8,7 +8,7 @@ module.exports = React.createClass({
     post = this.props.page.data;
 
     return (
-      <DocumentTitle title={`${post.title} | ${this.props.config.siteTitle}`}>
+      <DocumentTitle title={`${this.props.config.siteTitle}`}>
         <div className="markdown">
           <h2 style={{textAlign: "center"}}>{post.title}</h2>
           <div dangerouslySetInnerHTML={{__html: post.body}}/>
