570c3236764b866025ea386934441ba1f6cfb46f
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-07-16 10:46:08 +0300

    Support for multiple replit embed on one page

--- pages/en/developers/_components.jsx
+++ pages/en/developers/_components.jsx
@@ -114,15 +114,30 @@ const getArchitectureHeader = (props) => {
 }
 
 const getReplitEmbed = (props) => {
-  if (typeof props.replit == "undefined" || !props.replit.url || !props.replit.height) {
+  if (typeof props.replit == "undefined") {
     return (<span></span>);
   }
-  
+
+  const getEmbed = (example) => {
+     if (props.replit[example].height == null || props.replit[example].url == null) {
+       return null;
+     }
+
+     return (
+              <div>
+                <h3>{ example }</h3>
+                <p>{ props.replit[example].description }</p>
+                <iframe height={props.replit[example].height} width="100%" src={props.replit[example].url + "?lite=true"} frameBorder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
+              </div>
+            );
+  };
+
+  const list = Object.keys(props.replit).map(getEmbed);  
+
   return (
 	<div>
-	<h2>Test the API</h2>
-	<p>{props.replit.description}</p>
-	<iframe height={props.replit.height} width="100%" src={props.replit.url + "?lite=true"} frameBorder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
+	  <h2>Test the API</h2>
+          { list }
 	</div>);
 }
 
