f840c0ce3f27307f8a618ce1284ed4f67dfd95f5
Joel Lappalainen <lappalj8@gmail.com>
2021-09-08 23:10:00 +0300

    Update header images

--- src/components/FrontPageHeader.js
+++ src/components/FrontPageHeader.js
@@ -3,13 +3,10 @@ import styled from "styled-components"
 
 import digitransitImage from "../pages/digitransit-image.png"
 import digitransitImage2x from "../pages/digitransit-image-2x.png"
+import digitransitImageMobile from "../pages/digitransit-image-mobile.png"
 import hslLogo from "../pages/hsl.svg"
 import traficomLogo from "../pages/traficom.svg"
 import lmjLogo from "../pages/lmj.svg"
-
-import header from "../pages/header.jpg";
-import header2x from "../pages/header.2x.jpg";
-import headerMobile from "../pages/header.mobile.jpg";
 
 const HeaderImage = styled.div`
     position: relative;
@@ -30,54 +27,9 @@ const HeaderImage = styled.div`
     }
 
     @media (max-width: 600px) {
-        background-image: url(${digitransitImage});
+        background-image: url(${digitransitImageMobile});
         background-size: 600px 750px;
         height: 750px;
-    }
-`
-
-const HeaderLogo = styled.img`
-    position: absolute;
-    left: 13%;
-    top: 272px;
-    width: 338px;
-    height: 108px;
-
-    @media (max-width: 1024px) {
-        left: 8%;
-        top: 370px;
-        width: 250px;
-        height: 80px;
-    }
-
-    @media (max-width: 600px) {
-        left: 40px;
-        top: 440px;
-    }
-`
-
-const HeaderText = styled.h1`
-    position: absolute;
-    color: #fff;
-    font-weight: 300;
-    -webkit-font-smoothing: antialiased;
-    text-align: left;
-    left: 13%;
-    top: 415px;
-    max-width: 430px;
-
-    @media (max-width: 1024px) {
-        left: 8%;
-        top: 470px;
-        max-width: 300px;
-    }
-
-    @media (max-width: 600px) {
-        left: 40px;
-        top: 540px;
-        max-width: 250px;
-        font-size: 1.6rem;
-        line-height: 1.25;
     }
 `
 
